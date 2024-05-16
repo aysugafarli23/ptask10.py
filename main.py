b8fde8f980af901f59bd2f1bba6a95ec1f5200cf
Aysu Gafarli <aysuqafarli02@gmail.com>
2024-05-16 09:27:47 +0400

    New version

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-# with open("example.txt", encoding="utf-8", mode = "x") as data:
-    # data.write("The weather is so cold today \n so that I wanna wear thick clothes)")
-   
-with open("example.txt", encoding="utf-8", mode = "r") as file:
-    content = file.read()
-    
-with open("second.txt", encoding = "utf-8", mode = "w") as second:
-    second.write(content)
-    
-with open("second.txt", encoding="utf-8", mode = 'r') as second:
-    print(second.read())
+with open("input.txt", encoding = "utf-8", mode = "x") as file:
+    file.writelines(["1.Hello world ", "\n2.I'm the second line", "\nI'm the 3rd line"])
 
 
+
+with open("input.txt","r") as f:
+    line = f.readline()
+    formatted_line = line.upper()
+    
+
+ 
+with open("output.txt", "w") as g:
+    g.write(formatted_line)
